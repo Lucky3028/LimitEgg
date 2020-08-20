9aaf90c6f1f950561f269cd4a2eb5b6465bd0e53
Lucky3028 <lucky3028.0@gmail.com>
2020-08-20 22:15:10 +0900

    [Add] 基本機能の実装

--- src/main/kotlin/com/lucky3028/limitegg/LimitEgg.kt
+++ src/main/kotlin/com/lucky3028/limitegg/LimitEgg.kt
@@ -1,9 +1,17 @@
 package com.lucky3028.limitegg
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class LimitEgg : JavaPlugin() {
+    companion object {
+        var instance: LimitEgg? = null
+            private set
+    }
+
     override fun onEnable() {
+        Bukkit.getPluginManager().registerEvents(PlayerClickListener, this)
+        instance = this
         // Plugin startup logic
         // Playerが卵またはスポーンエッグを右クリックで使ったとき、それが自分がOwnerであるかMemberである保護であれば実行する。
     }
