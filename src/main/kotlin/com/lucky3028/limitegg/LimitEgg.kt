267f338f54d578efdac29cc89a0656e39aac6b9e
Lucky3028 <lucky3028.0@gmail.com>
2020-08-21 22:28:13 +0900

    [Add] Config系のまとめ

--- src/main/kotlin/com/lucky3028/limitegg/LimitEgg.kt
+++ src/main/kotlin/com/lucky3028/limitegg/LimitEgg.kt
@@ -1,10 +1,22 @@
 package com.lucky3028.limitegg
 
+import com.lucky3028.limitegg.Config.loadConfig
+import com.lucky3028.limitegg.Config.saveDefConfig
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class LimitEgg : JavaPlugin() {
+    companion object {
+        var instance: LimitEgg? = null
+            private set
+    }
+
     override fun onEnable() {
+        instance = this
+
         Bukkit.getPluginManager().registerEvents(PlayerClickListener, this)
+
+        saveDefConfig()
+        loadConfig()
     }
 }
