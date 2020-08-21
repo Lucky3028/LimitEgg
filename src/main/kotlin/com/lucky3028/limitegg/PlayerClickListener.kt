e53fd1a427c453a433350004edb3bfa6b5e63833
Lucky3028 <lucky3028.0@gmail.com>
2020-08-21 20:04:23 +0900

    [Fix] 保護がないところでもイベントをキャンセルする

--- src/main/kotlin/com/lucky3028/limitegg/PlayerClickListener.kt
+++ src/main/kotlin/com/lucky3028/limitegg/PlayerClickListener.kt
@@ -19,16 +19,14 @@ object PlayerClickListener : Listener {
 
         // pLayerが今立っている場所の保護を取得
         val regions = wg.getRegionManager(player.world).getApplicableRegions(player.location).regions
-        // 保護がなければ終了
-        if (regions.size < 1) return
 
         for (rg in regions) {
-            // 1つでもOwnerでもMemberでもない保護があるならイベントをキャンセルしその場で終了
+            // 1つでもOwnerまたはMemberである保護があるなら処理を終了
             // （isMemberは指定したLocalPlayerがその保護のMemberであるかだけでなくOwnerであるかどうかも調べてくれる）
-            if (!rg.isMember(wg.wrapPlayer(player))){
-                e.isCancelled = true
-                return
-            }
+            if (rg.isMember(wg.wrapPlayer(player))) return
         }
+
+        // まとめ：今いる座標に保護がないか、自分がOwnerでもMemberでもない保護が1つでもあればイベントをキャンセルする
+        e.isCancelled = true
     }
 }
